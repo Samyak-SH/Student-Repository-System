685327d24b79d0237b2a97148863495e67099815
shubam <chshivam148@gmail.com>
2025-05-02 17:48:35 +0530

    Updated studentcredentials

--- client/src/components/teacher/StudentCredentialForm.jsx
+++ client/src/components/teacher/StudentCredentialForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { FiUser, FiMail, FiLock, FiBookmark, FiX } from 'react-icons/fi'
-import { departments } from '../../utils/mockData'       // for now fetching department from MockData file later on it is replace by user auth
+import { departments } from '../../utils/mockData'       // for now fetching department from MockData file later on it is replace by real API call
 import { motion } from 'framer-motion'
 
 const StudentCredentialForm = ({ onClose, onSuccess }) => {
